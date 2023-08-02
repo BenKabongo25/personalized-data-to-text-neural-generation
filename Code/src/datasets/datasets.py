e0815449e120838e1a2b2559f89bdf4825ceb08c
Ben Kabongo <62559968+BenKabongo25@users.noreply.github.com>
2023-08-02 14:50:47 +0200

    update datasets.py

--- Code/src/datasets/datasets.py
+++ Code/src/datasets/datasets.py
@@ -1,8 +1,148 @@
+# Ben Kabongo
+# Personnalized data-to-text neural generation
+# ISIR/MLIA, 2023
+
+# Création des datasets
+
 import numpy as np
+import os
 import pandas as pd
 import sys
 
 BASE_DIR = '../../../Data/'
+
+
+
+def movies_critics():
+    """ Création des datasets movies_input et users_output 
+    movies_input : formattage des données de films de Rotten Tomatoes pour du data-to-text
+    users_output : formattage des critiques utilisateurs de Rotten Tomatoes
+    """
+
+    # chargement des datasets de Rotten Tomatoes
+    critic_reviews_filename = BASE_DIR + 'rotten/rotten_tomatoes_critic_reviews.csv'
+    movies_filename = BASE_DIR + 'rotten/rotten_tomatoes_movies.csv'
+
+    critic_reviews_df = pd.read_csv(critic_reviews_filename)
+    movies_df = pd.read_csv(movies_filename)
+
+    # Création du dataset movies
+    movies_input_df = pd.DataFrame()
+    movies_input_df['movieID'] = movies_df['rotten_tomatoes_link']
+    movies_input_df['input1'] = movies_df.apply(movie2inputformat1, axis=1)
+    movies_input_df['input2'] = movies_df.apply(movie2inputformat2, axis=1)
+    movies_input_df['input3'] = movies_df.apply(movie2inputformat3, axis=1)
+    movies_input_df.to_csv(BASE_DIR + 'appdata/rotten/movies_input.csv', index=True)
+
+    # Création du dataset des users
+    users_output_df = pd.DataFrame()
+    users_output_df['movieID'] = critic_reviews_df['rotten_tomatoes_link']
+    users_output_df['userID'] = critic_reviews_df['critic_name'].apply(to_userID)
+    users_output_df['rating'] = critic_reviews_df['review_score'].apply(to_rating)
+    users_output_df['target'] = critic_reviews_df['review_content']
+    users_output_df.to_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index=True)
+
+
+
+def wikiroto():
+    """ Merge du jeu de données WikiRoto et Rotten Tomatoes 
+    Pour le data-to-text personnalisé
+    """
+    wikiroto_dir = BASE_DIR + 'wikiroto/'
+
+    train_df = pd.read_csv(wikiroto_dir + 'data/wikiroto_train_with_table.csv')
+    test_df = pd.read_csv(wikiroto_dir + 'data/wikiroto_test_with_table.csv')
+    eval_df = pd.read_csv(wikiroto_dir + 'data/wikiroto_eval_with_table.csv')
+
+    wikiroto_df = pd.concat([train_df, test_df, eval_df])
+    wikiroto_df = wikiroto_df[['new_input', 'target', 'table', 'movie_info']]
+    wikiroto_df = wikiroto_df.rename({'new_input': 'input', 'table':'parent'}, axis=1)
+    wikiroto_df = wikiroto_df.reset_index(drop=True)
+    wikiroto_df = wikiroto_df.drop_duplicates()
+    wikiroto_df['title'] = wikiroto_df['input'].apply(get_title)
+    wikiroto_df['movie_info'] = wikiroto_df['movie_info'].str.strip()
+    wikiroto_df = wikiroto_df.dropna(axis=0)
+
+    movies_rotten_df = pd.read_csv(BASE_DIR + 'rotten/rotten_tomatoes_movies.csv', index_col=0)
+    movies_rotten_df = movies_rotten_df.drop_duplicates()
+    movies_rotten_df = movies_rotten_df[['movie_title', 'movie_info']]
+    movies_rotten_df = movies_rotten_df.rename({'movie_title': 'title'}, axis=1)
+    movies_rotten_df['title'] = movies_rotten_df['title'].str.strip()
+    movies_rotten_df['movie_info'] = movies_rotten_df['movie_info'].str.strip()
+    movies_rotten_df = movies_rotten_df.dropna(axis=0)
+    movies_rotten_df = movies_rotten_df.reset_index()
+
+    final_wikiroto_df = movies_rotten_df.merge(wikiroto_df, on=['title', 'movie_info'])
+    final_wikiroto_df = final_wikiroto_df.rename({'rotten_tomatoes_link': 'movieID'}, axis=1)
+    final_wikiroto_df = final_wikiroto_df.set_index('movieID')[['title', 'input', 'target', 'parent']]
+    final_wikiroto_df.to_csv(BASE_DIR + 'appdata/rotten/wikiroto_all.csv', index=True)
+
+
+
+def reviews_authors_split(n_authors=2):
+    """ Création de jeu de données (train, test, eval) de reviews avec leurs auteurs
+    Utile pour la tâche d'authorship attribution
+
+    :param n_authors: nombre d'auteurs
+    """
+    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
+    users_output_df = users_output_df[['userID', 'movieID', 'target']]
+    users_output_df = users_output_df[users_output_df['userID'] != 'u/nan']
+    users_output_df = users_output_df.dropna()
+
+    occurrences = users_output_df['userID'].value_counts()[:n_authors]
+    mask = users_output_df.userID.isin(occurrences.index)
+    df = users_output_df[mask]
+    df = df.rename({'target': 'review'}, axis=1)
+    df = df[['review', 'movieID', 'userID']]
+    
+    sample_size = occurrences.min()
+    train_dfs, test_dfs, eval_dfs = [], [], []
+
+    for ui, u in enumerate(df.userID.unique()):
+        user_df = df[df['userID'] == u]
+        sample_df = user_df.sample(n=sample_size, replace=True)
+        sample_df['userNum'] = [ui]*sample_size
+        
+        train_df = sample_df.sample(frac=.8)
+        test_eval_df = sample_df.drop(train_df.index).reset_index(drop=True)
+        train_df = train_df.reset_index(drop=True)
+        test_df = test_eval_df.sample(frac=.5)
+        eval_df = test_eval_df.drop(test_df.index).reset_index(drop=True)
+        test_df = test_df.reset_index(drop=True)
+        
+        train_dfs.append(train_df)
+        test_dfs.append(test_df)
+        eval_dfs.append(eval_df)
+
+    train_df = pd.concat(train_dfs)
+    test_df = pd.concat(test_dfs)
+    eval_df = pd.concat(eval_dfs)
+        
+    return train_df, test_df, eval_df
+
+
+
+def to_strap_format(train_df, test_df, eval_df, n_authors=2):
+    """ Mise sous format STRAP de jeu de données de review-auteur
+    Utile pour le transfert de style
+
+    STRAP : https://github.com/martiansideofthemoon/style-transfer-paraphrase/tree/master
+
+    :param train_df
+    :param test_df
+    :param eval_df
+    :param n_authors : nombre d'auteurs
+    """
+    dt_dir = f'{BASE_DIR}strap/{n_authors}/' 
+    os.makedirs(dt_dir, exist_ok=True)
+
+    data = {'train': train_df, 'test': test_df, 'dev': eval_df}
+    for name, df in data.items():
+        with open(f'{dt_dir}{name}.txt', "w") as txt:
+            txt.write("\n".join(df["review"].tolist()))
+        with open(f'{dt_dir}{name}.label', "w") as label:
+            label.write("\n".join(df["userID"].tolist()))
 
 
 # renommage des features pour les infos des films
@@ -12,19 +152,22 @@ FEATURES_NAMES = {'movie_title': 'title' , 'movie_info': 'info', 'critics_consen
                   'audience_status': 'audience_status', 'audience_rating': 'audience_rating','audience_count': 'audience_count'}
 
 
-# formattage du nom des utilisateurs
+
 def to_userID(username):
+    """ formattage du nom des utilisateurs """
     return 'u/' + '_'.join(str(username).split())
 
 
-# normalisation des scores des critiques
-CINEMA_NOTATION_DICT = {'A+': 5, 'A': 4, 'A-': 3.7, 'B+': 3.3, 'B': 3,
-        'B-': 2.7, 'C+': 2.3, 'C': 2, 'C-': 1.7, 'D+': 1.3, 'D': 1, 'D-': 0.7, 'F': 0}
 
 def cinema_score(notation):
-    return CINEMA_NOTATION_DICT.get(notation, None)
+    """ Normalisation des notes de cinéma """
+    cinema_notations = {'A+': 5, 'A': 4, 'A-': 3.7, 'B+': 3.3, 'B': 3,
+        'B-': 2.7, 'C+': 2.3, 'C': 2, 'C-': 1.7, 'D+': 1.3, 'D': 1, 'D-': 0.7, 'F': 0}
+    return cinema_notations.get(notation, None)
+
 
 def to_rating(s):
+    """ Normalisation des notes de film de Rotten Tomatoes """
     try: 
         nd = s.split('/')
         if len(nd) == 2:
@@ -42,8 +185,8 @@ def to_rating(s):
         return None
 
 
-# input formats
 def movie2inputformat1(movie):
+    """ Format d'input pour le data-to-text """
     input_ = '<movie> '
     
     for att in FEATURES_NAMES.keys():
@@ -54,7 +197,9 @@ def movie2inputformat1(movie):
     input_ += '</movie>'
     return input_
 
+
 def movie2inputformat2(movie):
+    """ Format d'input pour le data-to-text """
     input_ = ''
     
     for att in FEATURES_NAMES.keys():
@@ -64,7 +209,9 @@ def movie2inputformat2(movie):
     
     return input_
 
+
 def movie2inputformat3(movie):
+    """ Format d'input pour le data-to-text """
     input_ = dict()
     
     for att in FEATURES_NAMES.keys():
@@ -73,7 +220,22 @@ def movie2inputformat3(movie):
     return input_
 
 
-def build_movie_n_reviews_to_review_dataset(n=10):
+def get_title(input1):
+    """ Récupération des titres dans les données wikiroto """
+    start = input1.index('<movie_title>')
+    end = input1.index('</movie_title>')
+    if start > 0 and end > 0:
+        start += len('<movie_title>')
+        return input1[start:end].strip()
+    return None
+
+
+# autres datasets
+
+def movie_n_reviews_to_review(n=10):
+    movies_input_df = pd.read_csv(BASE_DIR + 'appdata/rotten/movies_input.csv', index_col=0)
+    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
+
     users_output_filtered_df = users_output_df[['movieID', 'userID', 'target']]
     users_output_filtered_df.dropna(inplace=True)
 
@@ -101,7 +263,10 @@ def build_movie_n_reviews_to_review_dataset(n=10):
     return df
 
 
-def build_movie_review_to_rating_dataset():
+def movie_review_to_rating():
+    movies_input_df = pd.read_csv(BASE_DIR + 'appdata/rotten/movies_input.csv', index_col=0)
+    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
+
     users_output_filtered_df = users_output_df[['movieID', 'userID', 'target', 'rating']]
     users_output_filtered_df.dropna(inplace=True)
 
@@ -117,7 +282,9 @@ def build_movie_review_to_rating_dataset():
     return df
 
 
-def build_review_to_rating_dataset():
+def review_to_rating():
+    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
+
     users_output_filtered_df = users_output_df[['target', 'rating']]
     users_output_filtered_df.dropna(inplace=True)
 
@@ -127,7 +294,9 @@ def build_review_to_rating_dataset():
     return df
 
 
-def build_n_review_n_1_rating_user_to_rating_dataset(n):
+def n_review_n_1_rating_user_to_rating(n):
+    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
+
     users_output_filtered_df = users_output_df[['userID', 'target', 'rating']]
     users_output_filtered_df.dropna(inplace=True)
 
@@ -161,67 +330,6 @@ def build_n_review_n_1_rating_user_to_rating_dataset(n):
 
 
 
-def build_movie_n_review_n_1_rating_to_rating_dataset(n):
-    users_output_filtered_df = users_output_df[['movieID', 'userID', 'target', 'rating']]
-    users_output_filtered_df.dropna(inplace=True)
-
-
-
-CODE = int(sys.argv[1])
-
-movies_input_df = None
-users_output_df = None
-
-if CODE == 0:
-    # création des datasets movies_input et users_output
-
-    # chargement des datasets de Rotten Tomatoes
-    critic_reviews_filename = BASE_DIR + 'rotten/rotten_tomatoes_critic_reviews.csv'
-    movies_filename = BASE_DIR + 'rotten/rotten_tomatoes_movies.csv'
-
-    critic_reviews_df = pd.read_csv(critic_reviews_filename)
-    movies_df = pd.read_csv(movies_filename)
-
-    # Création du dataset movies
-    movies_input_df = pd.DataFrame()
-    movies_input_df['movieID'] = movies_df['rotten_tomatoes_link']
-    movies_input_df['input1'] = movies_df.apply(movie2inputformat1, axis=1)
-    movies_input_df['input2'] = movies_df.apply(movie2inputformat2, axis=1)
-    movies_input_df['input3'] = movies_df.apply(movie2inputformat3, axis=1)
-    movies_input_df.to_csv(BASE_DIR + 'appdata/rotten/movies_input.csv', index=True)
-
-    # Création du dataset des users
-    users_output_df = pd.DataFrame()
-    users_output_df['movieID'] = critic_reviews_df['rotten_tomatoes_link']
-    users_output_df['userID'] = critic_reviews_df['critic_name'].apply(to_userID)
-    users_output_df['rating'] = critic_reviews_df['review_score'].apply(to_rating)
-    users_output_df['target'] = critic_reviews_df['review_content']
-    users_output_df.to_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index=True)
-
-else:
-    # Chargement des datasets
-    movies_input_df = pd.read_csv(BASE_DIR + 'appdata/rotten/movies_input.csv', index_col=0)
-    users_output_df = pd.read_csv(BASE_DIR + 'appdata/rotten/users_output.csv', index_col=0)
-
-
-if CODE == 1:
-    # création du dataset movie_n_reviews_to_review
-    n = int(sys.argv[2])
-    df = build_movie_n_reviews_to_review_dataset(n)
-    df.to_csv(BASE_DIR + 'appdata/rotten/movie_n_reviews_to_review.csv', index=True)
-
-elif CODE == 2:
-    # création du dataset movie_review_to_rating
-    df = build_movie_review_to_rating_dataset()
-    df.to_csv(BASE_DIR + 'appdata/rotten/movie_review_to_rating.csv', index=True)
-
-elif CODE == 3:
-    # création du dataset review_to_rating
-    df = build_review_to_rating_dataset()
-    df.to_csv(BASE_DIR + 'appdata/rotten/review_to_rating.csv', index=True)
-
-
-elif CODE == 4:
-    # création du dataset movie_n_review_n_1_rating_to_rating
-    pass
-
+if __name__ == '__main__':
+    movies_critics()
+    wikiroto()
